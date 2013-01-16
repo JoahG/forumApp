31a2e83fa56ccd002f444de2ca93dfab8596129a
Joah Gerstenberg <joahd@gbergfamily.com>
2013-01-16 17:38:30 -0600

    Changed min password length

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
   validates_presence_of :email
   validates_uniqueness_of :email
   validates :email, :presence => true, :email => true
-  validates_length_of :password, :minimum => 8
+  validates_length_of :password, :minimum => 6
 
   def self.authenticate(email,password)
   	user = find_by_email(email)
