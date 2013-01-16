e70988f24b5ab204bebbc1a536f07ba5ad33e871
Joah Gerstenberg <joahd@gbergfamily.com>
2013-01-15 22:17:35 -0600

    Updated "Admin" Page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,8 +23,14 @@ class UsersController < ApplicationController
 
   def destroy
     @user = User.find(params[:id])
+    t = false
+    if current_user == @user
+      t = true
+    end
     @user.destroy
-    session[:user_id] = nil
+    if t
+      session[:user_id] = nil
+    end
     respond_to do |format|
       format.html { redirect_to root_url }
       format.json { head :no_content }
@@ -42,5 +48,19 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 end
