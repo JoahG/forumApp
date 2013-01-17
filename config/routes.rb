a80c8669c1022b2204ebb1ea810e3bddc279bada
Joah Gerstenberg <joahd@gbergfamily.com>
2013-01-17 15:12:31 -0600

    Fixed bug, added comment deletion functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ ForumApp::Application.routes.draw do
   get "log_in" => "sessions#new", :as => "log_in"
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "sign_up" => "users#new", :as => "sign_up"
-  get "comment" => "comments#new", :as => "comment"
   root :to => "posts#index"
   resources :users
   resources :sessions
