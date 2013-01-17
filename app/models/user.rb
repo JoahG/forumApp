159dd512ecc853a88c847ba022256b198fcd388d
Joah Gerstenberg <joahd@gbergfamily.com>
2013-01-17 09:00:32 -0600

    Changed posts/comments UI, changed .note formatting

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ActiveRecord::Base
   validates_presence_of :password, :on => :create
   validates_presence_of :email
   validates_uniqueness_of :email
+  validates_uniqueness_of :name
   validates :email, :presence => true, :email => true
   validates_length_of :password, :minimum => 6
 
