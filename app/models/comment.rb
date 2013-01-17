2bf52ae2e993a0fd77bc83a11089110c6baf4518
Joah Gerstenberg <joahd@gbergfamily.com>
2013-01-17 17:20:30 -0600

    Again, dedicated to Haley :)

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,8 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
   attr_accessible :content, :user_id, :post_id
-
+  validates_presence_of :content
+  validates_length_of :content, :minimum => 1
   before_save :render_body
 
   private
