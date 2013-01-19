46abac02246782f546d4ae420900e194cbd43688
Joah Gerstenberg <joahd@gbergfamily.com>
2013-01-19 08:48:17 -0600

    Updated min length for ncomment

--- app/models/ncomment.rb
+++ app/models/ncomment.rb
@@ -2,4 +2,6 @@ class Ncomment < ActiveRecord::Base
   belongs_to :user
   belongs_to :comment
   attr_accessible :comment_id, :content, :user_id
+  validates_presence_of :content
+  validates_length_of :content, :minimum => 1
 end
