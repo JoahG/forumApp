61371851c6cf37cead1c7cf27b0ecbd3b81a4ed0
Joah Gerstenberg <joahd@gbergfamily.com>
2013-01-21 11:28:53 -0600

    Added notifications for +1's

--- app/controllers/plusones_controller.rb
+++ app/controllers/plusones_controller.rb
@@ -13,6 +13,9 @@ class PlusonesController < ApplicationController
 
   	respond_to do |format|
   		if @plusone.save
+  			if @plusone.user != @plusone.comment.user
+	          Notification.create(:content => "<a href='/users/#{@plusone.user.id}'>#{@plusone.user.name}</a> +1'd your comment on '<a href='/posts/#{@plusone.comment.post.id}'>#{@plusone.comment.post.title}</a>'", :user_id => @plusone.comment.user.id)
+	        end
   			format.html {redirect_to @plusone.comment.post}
   		else
   			format.html {redirect_to @plusone.comment.post}
