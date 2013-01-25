2f5dd2ef563f94166dea181fc73e6999cdd425c5
Joah Gerstenberg <joahd@gbergfamily.com>
2013-01-24 22:09:39 -0600

    Nothing, really, I'm serious, I have no idea why it is even letting me do this….

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-
+    
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @posts }
